c5e7e8645571289b1b081c9ae07a0b56dafda7a8
GOM <gom@theba.se>
2021-08-23 11:38:28 +0200

    add tests and statements for ErrorF

--- err_test.go
+++ err_test.go
@@ -15,6 +15,10 @@ const (
 	sampleError3 = Error("sample error 3")
 )
 
+var (
+	sampleErrorF = ErrorF("test %s")
+)
+
 func TestError(t *testing.T) {
 	e := Error("test")
 	if e.Error() != "test" {
@@ -28,6 +32,36 @@ func TestError(t *testing.T) {
 	}
 	if e.WithParameters("something") != e {
 		t.Error("Plain Error should have WithParameters as a neutral operation")
+	}
+}
+
+func TestErrorF(t *testing.T) {
+	ef := ErrorF("test %s")
+	ef2 := ErrorF("test %s")
+	if ef.Equals(ef2) {
+		t.Error("Error kinds should only be equal to itself")
+	}
+	if !ef.Equals(ef) {
+		t.Error("Error kinds should be equal to itself")
+	}
+	e := ef.WithParameters("something").(IError)
+	if e.Error() != "test something" {
+		t.Error("Error message not expected", e.Error())
+	}
+	if !e.Equals(Error("test something")) {
+		t.Error("Error equals should succeed on errors with the same message")
+	}
+	if !e.Equals(ef) {
+		t.Error("Error equals should succeed comparing with the kind of error")
+	}
+	if e.Equals(Error("test other")) {
+		t.Error("Error equals should fail for any other error")
+	}
+	if e.Equals(ef2) {
+		t.Error("Error equals should fail comparing with any other error kind")
+	}
+	if e.WithParameters("something") == e {
+		t.Error("With parameters should result in a different error instance")
 	}
 }
 
@@ -47,7 +81,10 @@ func TestErrors(t *testing.T) {
 	if es.WithParameters("something") != es {
 		t.Error("Errors should have WithParameters as a neutral operation")
 	}
-	if es.Count() != 2 {
+
+	es.AddError(sampleErrorF.WithParameters("something").(IError))
+
+	if es.Count() != 3 {
 		t.Error("Reporting incorrect number of errors :", es.Count())
 	}
 	t.Run("Test if errors contains an error", func(t *testing.T) {
@@ -59,6 +96,9 @@ func TestErrors(t *testing.T) {
 		}
 		if es.Contains(sampleError3) {
 			t.Error("Errors contains sample 3")
+		}
+		if !es.Contains(sampleErrorF) {
+			t.Error("Errors doesn't contain sample errorF")
 		}
 	})
 
@@ -72,6 +112,9 @@ func TestErrors(t *testing.T) {
 		if es.Contains(Error("sample error 3")) {
 			t.Error("Errors contains sample 3")
 		}
+		if !es.Contains(Error("test something")) {
+			t.Error("Errors doesn't containt errorf")
+		}
 	})
 }
 
@@ -80,6 +123,7 @@ func ExampleErrors() {
 	es.AddError(sampleError1)
 	es.AddError(sampleError2)
 	es.AddError(sampleError3)
+	es.AddError(sampleErrorF.WithParameters("something").(IError))
 
 	fmt.Print(es.Error())
 
@@ -88,4 +132,5 @@ func ExampleErrors() {
 	// sample error 1
 	// sample error 2
 	// sample error 3
+	// test something
 }
