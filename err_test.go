db34e46dfa5547b30d98dc1e6abf6a9bf0674bab
GOM <gom@theba.se>
2022-09-29 10:13:42 +0200

    have tests done in a proper test package

--- err_test.go
+++ err_test.go
@@ -2,22 +2,24 @@
 // Since 25/06/2021 By GOM
 // Licensed under MIT License
 
-package err
+package err_test
 
 import (
 	"fmt"
 	"testing"
+
+	"github.com/gomatbase/go-error"
 )
 
 const (
-	sampleError1 = Error("sample error 1")
-	sampleError2 = Error("sample error 2")
-	sampleError3 = Error("sample error 3")
-	sampleErrorF = ErrorF("test %s")
+	sampleError1 = err.Error("sample error 1")
+	sampleError2 = err.Error("sample error 2")
+	sampleError3 = err.Error("sample error 3")
+	sampleErrorF = err.ErrorF("test %s")
 )
 
 func TestError(t *testing.T) {
-	e := Error("test")
+	e := err.Error("test")
 	if e.Error() != "test" {
 		t.Error("Error message not expected", e.Error())
 	}
@@ -35,13 +37,13 @@ func TestErrorF(t *testing.T) {
 	if !sampleErrorF.IsKindOf(e) {
 		t.Error("Error is not of the expected kind")
 	}
-	if !ErrorF("test %s").IsKindOf(e) {
+	if !err.ErrorF("test %s").IsKindOf(e) {
 		t.Error("Error is not of the expected kind")
 	}
 }
 
 func TestErrors(t *testing.T) {
-	es := Errors()
+	es := err.Errors()
 	es.AddError(sampleError1)
 	es.Add("sample error 2")
 
@@ -66,68 +68,68 @@ func TestErrors(t *testing.T) {
 	})
 
 	t.Run("Test if errors contains an error message", func(t *testing.T) {
-		if !es.Contains(Error("sample error 1")) {
+		if !es.Contains(err.Error("sample error 1")) {
 			t.Error("Errors doesn't contain sample 1")
 		}
-		if !es.Contains(Error("sample error 2")) {
+		if !es.Contains(err.Error("sample error 2")) {
 			t.Error("Errors doesn't contain sample 2")
 		}
-		if es.Contains(Error("sample error 3")) {
+		if es.Contains(err.Error("sample error 3")) {
 			t.Error("Errors contains sample 3")
 		}
-		if !es.Contains(ErrorF("test %s")) {
+		if !es.Contains(err.ErrorF("test %s")) {
 			t.Error("Errors doesn't containd errorf")
 		}
-		if es.Contains(Error("test something")) {
+		if es.Contains(err.Error("test something")) {
 			t.Error("Errors identifies errorF instance as an error instance")
 		}
 	})
 }
 
 func TestIsContainedIn(t *testing.T) {
-	es := Errors()
+	es := err.Errors()
 	es.AddError(sampleError1)
 	efInstance := sampleErrorF.WithValues("something")
 	es.AddError(efInstance)
 
-	if !IsContainedIn(sampleError1, es) {
+	if !err.IsContainedIn(sampleError1, es) {
 		t.Error("sample error 1 should be contained in errors")
 	}
-	if IsContainedIn(sampleError2, es) {
+	if err.IsContainedIn(sampleError2, es) {
 		t.Error("sample error 2 should not be contained in errors")
 	}
-	if !IsContainedIn(sampleErrorF, es) {
+	if !err.IsContainedIn(sampleErrorF, es) {
 		t.Error("sample error f should be contained in errors")
 	}
-	if !IsContainedIn(sampleErrorF, efInstance) {
+	if !err.IsContainedIn(sampleErrorF, efInstance) {
 		t.Error("sample error f should be contained in error f instance")
 	}
-	if !IsContainedIn(sampleError1, Error("sample error 1")) {
+	if !err.IsContainedIn(sampleError1, err.Error("sample error 1")) {
 		t.Error("sample error 1 should be contained in sample error 1")
 	}
-	if IsContainedIn(sampleError1, sampleError2) {
+	if err.IsContainedIn(sampleError1, sampleError2) {
 		t.Error("sample error 1 should not be contained in sample error 2")
 	}
 }
 
 func TestCount(t *testing.T) {
-	es := Errors()
+	es := err.Errors()
 	es.AddError(sampleError1)
 	es.AddError(sampleError2)
 
-	if Count(nil) != 0 {
+	if err.Count(nil) != 0 {
 		t.Error("null errors should result in a 0 count")
 	}
-	if count := Count(es); count != 2 {
+	if count := err.Count(es); count != 2 {
 		t.Error("Count of errors not the expected amount :", count)
 	}
-	if Count(sampleError2) != 1 {
+	if err.Count(sampleError2) != 1 {
 		t.Error("Count of any error should result in 1")
 	}
 }
 
 func ExampleErrors() {
-	es := Errors()
+	es := err.Errors()
 	es.AddError(sampleError1)
 	es.AddError(sampleError2)
 	es.AddError(sampleError3)
